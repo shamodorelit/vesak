284c05187fa3209c060b3576f396c335161f4e92
shamodorelit <shamodorelit@gmail.com>
2026-05-26 21:54:52 +0530

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-
+import { MusicPlayer } from "@/components/MusicPlayer";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -28,6 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <MusicPlayer />
         </ThemeProvider>
       </body>
     </html>
